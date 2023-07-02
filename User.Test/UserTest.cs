7d33af357258ba8fe2032a37e00c3a6d22ed5038
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2023-07-02 18:09:36 +0600

    Added necessary layers

---
+++ User.Test/UserTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace User.Test
+{
+    public class UserTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenTypeUserId_ThenGetUserData()
+        {
+            
+        }
+    }
+}
